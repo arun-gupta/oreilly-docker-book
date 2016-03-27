7c1c99421929b2ec094122c26ce3310209381e87
Arun Gupta <arun.gupta@gmail.com>
2016-03-26 21:16:32 -0700

    adding a new sample to show docker-maven-plugin

---
+++ hello-maven/src/main/java/com/oreilly/example/docker/maven/App.java
@@ -0,0 +1,13 @@
+package com.oreilly.example.docker.maven;
+
+/**
+ * Hello world!
+ *
+ * @author Arun Gupta
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
