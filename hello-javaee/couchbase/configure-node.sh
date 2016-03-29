f602547d1529b388924dc5b980ad5388f2b7b93a
Arun Gupta <arun.gupta@gmail.com>
2016-03-28 18:33:23 -0700

    adding a new bucket and creating index

--- hello-javaee/couchbase/configure-node.sh
+++ hello-javaee/couchbase/configure-node.sh
@@ -7,7 +7,9 @@ sleep 15
 curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
 curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
 curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=Administrator -d password=password
-curl -v -u Administrator:password -X POST http://127.0.0.1:8091/pools/default/buckets -d name=docker-oreilly -d bucketType=couchbase -d ramQuotaMB=200 -d authType=none -d proxyPort=11216
+curl -v -u Administrator:password -X POST http://127.0.0.1:8091/pools/default/buckets -d name=books -d bucketType=couchbase -d ramQuotaMB=200 -d authType=sasl
+sleep 10
+curl -v http://127.0.0.1:8093/query/service -d 'statement=create primary index on books'
 
 fg 1
 
